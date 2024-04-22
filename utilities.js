372f0942961218213e9b592fd6c599b664490b71
Niwahan <aniwahang@gmail.com>
2024-04-22 13:03:02 +0545

    Added more media to post

--- utilities.js
+++ utilities.js
@@ -1,5 +1,6 @@
 const request = require("request");
 const fs = require("fs");
+const path = require("path");
 
 const download = function (uri, filename, callback) {
   request.head(uri, function (err, res, body) {
@@ -7,4 +8,17 @@ const download = function (uri, filename, callback) {
   });
 };
 
-module.exports = { download };
+const getRandomImage = () => {
+  const directory = "./images";
+  const files = fs.readdirSync(directory);
+
+  const imageFiles = files.filter((file) =>
+    /\.(jpg|jpeg|png|gif)$/i.test(file)
+  );
+
+  const randomIndex = Math.floor(Math.random() * imageFiles.length);
+
+  return path.join(directory, imageFiles[randomIndex]);
+};
+
+module.exports = { download, getRandomImage };
